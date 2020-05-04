45a856a25dd23bd842be611b71804b3dcf8049fd
Chaitali1307 <chatali1307@gmail.com>
2020-05-04 12:32:00 +0530

    Day7 Array

--- 30DaysOfCode/src/com/code/day7/ArraysTest.java
+++ 30DaysOfCode/src/com/code/day7/ArraysTest.java
@@ -12,16 +12,16 @@ public class ArraysTest {
 
 
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int n = sc.nextInt();
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int[] arr = new int[n];
 
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String[] arrItems = sc.nextLine().split(" ");
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < n; i++) {
             int arrItem = Integer.parseInt(arrItems[i]);
@@ -31,6 +31,6 @@ public class ArraysTest {
             System.out.print(arr[i]+" ");
          }
 
-        scanner.close();
+        sc.close();
     }
 }
