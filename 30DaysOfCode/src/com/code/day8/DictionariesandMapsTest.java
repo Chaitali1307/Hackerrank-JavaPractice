fed392b1d58e6f733bee1b414145f7cbf8a7843b
Chaitali1307 <chatali1307@gmail.com>
2020-05-06 21:19:39 +0530

    Day 8 & 9 commit

--- 30DaysOfCode/src/com/code/day8/DictionariesandMapsTest.java
+++ 30DaysOfCode/src/com/code/day8/DictionariesandMapsTest.java
@@ -1,5 +1,34 @@
 package com.code.day8;
 
-public class DictionariesandMapsTest {
+import java.util.*;
+import java.io.*;
 
+public class DictionariesandMapsTest{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String,Integer> phoneBook = new HashMap<String,Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            phoneBook.put(name,phone);
+        }
+        while(in.hasNext()){
+          String s = in.next();
+            // Write code here
+            if(phoneBook.containsKey(s)==true)
+            {
+                System.out.println(s+"="+phoneBook.get(s));
+                
+            }
+            else
+            {
+                System.out.println("Not found");
+               
+            }
+        }
+        in.close();
+    }
 }
+
